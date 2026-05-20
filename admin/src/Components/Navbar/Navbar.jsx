7569a28e7003e351a448dd46961203736430d826
gunaindia1983-bit <gunaindia1983@gmail.com>
2026-05-20 12:16:23 -0400

    Update Navbar.jsx

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import './Navbar.css'
-import {assets} from '../../assets/assets'
+// import React from 'react'
+// import './Navbar.css'
+// import {assets} from '../../assets/assets'
 
-const Navbar = () =>{
-    return(
+// const Navbar = () =>{
+//     return(
+//         <div className="navbar">
+//             <img src={assets.logo} alt="" className="logo" />
+//             <img src={assets.profile_image} alt="" className="profile" />
+
+//         </div>
+//     )
+// }
+
+// export default Navbar;
+
+import React,{useContext} from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+import {toast} from 'react-toastify';
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+
+    const navigate = useNavigate();
+    const {token,setToken,setShowLogin}=useContext(StoreContext);
+
+
+    const logout=()=>{
+        localStorage.removeItem("token");
+        setToken("");
+        setShowLogin(true);
+        toast.success("Logged-out Successfully");
+        navigate('/');
+
+    }
+    return (
         <div className="navbar">
-            <img src={assets.logo} alt="" className="logo" />
-            <img src={assets.profile_image} alt="" className="profile" />
+            <img src={assets.logo2} alt="" className="logo" />
+            <div className="navbar-profile">
+                <img src={assets.profile_image} alt="" className="profile" />
+                <ul className="navbar-profile-dropdown">
+                    <li onClick={logout}>
+                        <img src={assets.logout_icon} alt="" />
+                        <p>Signout</p></li>
+                </ul>
+            </div>
 
         </div>
     )
