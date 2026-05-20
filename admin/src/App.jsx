f520b5cec34317dcca00b8b1f0c0dcf91bad3671
gunaindia1983-bit <gunaindia1983@gmail.com>
2026-05-20 12:26:54 -0400

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Sidebar from './Components/Sidebar/SIdebar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import { Routes, Route } from 'react-router-dom';
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
