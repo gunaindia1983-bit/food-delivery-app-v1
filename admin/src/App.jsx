1f3daa78c7cc2d08ec7d6878def035c1c7c6fc1c
gunaindia1983-bit <gunaindia1983@gmail.com>
2026-05-20 12:05:40 -0400

    Refactor App component to include new routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Sidebar from './Components/Sidebar/SIdebar';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
 import Orders from './pages/Orders/Orders';
- import { ToastContainer } from 'react-toastify';
+import Comments from './pages/Comments/Comments';
+import Sentiments from './pages/Sentiment/Sentiments';
+import { ToastContainer } from 'react-toastify';
+import LoginPopup from './Components/LoginPopup/LoginPopup';
+import { StoreContext } from './context/StoreContext';
 
 const App = () => {
-  const url = 'http://localhost:4000'
-  return(
-    <div className="">
-      <ToastContainer />
+
+  const { showLogin, setShowLogin, url } = useContext(StoreContext);
+  
+
+
+  return (
+
+    <>
+      {showLogin ? <LoginPopup /> : <></>}
+      <ToastContainer autoClose={1000} />
       <Navbar />
       <hr />
       <div className="app-content">
-        <Sidebar />
+        <Sidebar /> 
         <Routes>
           <Route path='/add' element={<Add url={url} />} />
-           <Route path='/list' element={<List url={url} />} />
-            <Route path='/orders' element={<Orders url={url} /> }/>
+          <Route path='/list' element={<List url={url} />} />
+          <Route path='/orders' element={<Orders url={url} />} />
+          <Route path='/comments' element={<Comments url={url} />} />
+          <Route path='/sentiments' element={<Sentiments url={url} />} />
         </Routes>
       </div>
-    </div>
+    </>
   )
 }
 
